ccea7088769fef70188d75063a067876b775de00
matheus-pissaia <pissaia.matheus03@gmail.com>
2023-05-08 21:27:22 -0300

    feat: adicionado classe `AbstractTela`

--- telas/abstractTela.py
+++ telas/abstractTela.py
@@ -1,3 +1,20 @@
-class AbstractTela:
-    def teste(self):
-        pass
+from abc import ABC
+
+
+class AbstractTela(ABC):
+    def verifica_opcao(mensagem: str, opcoes_validas: list[int]):
+        while True:
+            opcao_selecionada = int(input(mensagem))
+
+            try:
+                if opcao_selecionada not in opcoes_validas:
+                    raise ValueError
+                
+                return opcao_selecionada
+            
+            except ValueError:
+                print("Valor incorreto: Digite um valor válido!")
+                print("Opções válidas:", opcoes_validas)
+
+    def mostra_mensagem(mensagem: int):
+        print(mensagem)
