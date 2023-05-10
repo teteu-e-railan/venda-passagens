b9731ea599762573f5cdefce8fda5b21b4414028
RAILANABREU <railandeabreu@gmail.com>
2023-05-09 22:28:06 -0300

    feat: telas do aviao (WIP)

---
+++ telas/telaAviao.py
@@ -0,0 +1,38 @@
+from controladores.controladorAviao import ControladorAviao
+
+
+class TelaAviao():
+
+    def tela_opcoes(self):
+        print("-------- Aviões ----------")
+        print("Escolha a opcao")
+        print("1 - Incluir Avião")
+        print("2 - Alterar Avião")
+        print("3 - Listar Aviões")
+        print("4 - Excluir Avião")
+        print("0 - Retornar")
+
+        opcao = int(input("Escolha a opcao: "))
+        return opcao
+    
+    
+    def pega_dados_aviao(self):
+        print("-------- DADOS AVIÃO ----------")
+        modelo = input("Modelo: ")
+        fileiras = input("Fileiras: ")
+        assentos_por_fileira = input("Assentos por fileira: ")
+
+        return {"modelo": modelo, "Fileiras": fileiras, "assentos_por_fileira": assentos_por_fileira}
+    
+    def mostra_amigo(self, dados_aviao):
+        print("Modelo do Avião: ", dados_aviao["modelo"])
+        print("fileiras: ", dados_aviao["fileiras"])
+        print("assentos por fileira: ", dados_aviao["assento_por_fileiras"])
+        print("\n")
+    
+    def seleciona_aviao(self):
+        codigo = input("Codigo do avião que deseja selecionar: ")
+        return codigo
+    
+    def mostra_mensagem(self, msg):
+        print(msg)
