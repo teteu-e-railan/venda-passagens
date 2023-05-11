6619e0cbf550a4758c41d3b8a56016fa009d178d
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-05-11 00:09:30 -0300

    feat: adicionado um construtor abstrato para inicializar a tela com um dicionário de opcoes

--- telas/abstractTela.py
+++ telas/abstractTela.py
@@ -1,20 +1,33 @@
-from abc import ABC
+from abc import ABC, abstractmethod
 
 
 class AbstractTela(ABC):
-    def verifica_opcao(self, mensagem: str, opcoes_validas: "list[int]") -> int:
+    @abstractmethod
+    def __init__(self, opcoes: "dict[int, str]"):
+        self.__opcoes = opcoes
+        self.__lista_de_opcoes_validas = list(opcoes.keys())
+
+    @property
+    def opcoes(self):
+        return self.__opcoes
+
+    @property
+    def lista_de_opcoes_validas(self):
+        return self.__lista_de_opcoes_validas
+
+    def verifica_opcao(self, mensagem: str) -> int:
         while True:
             opcao_selecionada = int(input(mensagem))
 
             try:
-                if opcao_selecionada not in opcoes_validas:
+                if opcao_selecionada not in self.lista_de_opcoes_validas:
                     raise ValueError
 
                 return opcao_selecionada
 
             except ValueError:
                 print("Valor incorreto: Digite um valor válido!")
-                print("Opções válidas:", opcoes_validas)
+                print("Opções válidas:", self.lista_de_opcoes_validas)
 
     def mostra_mensagem(self, mensagem: int):
         print(mensagem)
