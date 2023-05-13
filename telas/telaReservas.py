a85bb1ee76e3e907d024a55ab4ff5cb634c49dcf
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-05-13 02:31:16 -0300

    feat: implementacao da tela de reservas

--- telas/telaReservas.py
+++ telas/telaReservas.py
@@ -14,12 +14,38 @@ class TelaReservas(AbstractTela):
         )
 
     def mostra_opcoes(self):
-        print("-------- Sistema ----------")
+        print("-------- Reservas ----------")
 
         for index, opcao in self.opcoes.items():
             print(f"{index} - {opcao}")
 
         return self.verifica_opcao("Escolha uma opção: ")
 
-    def mostra_reserva(self, dados_reserva: "dict[int, str]"):
-        pass
+    def pega_dados_reserva(self):
+        assento = input("Digite o assento desejado: ")
+        # TODO verificar se assento está disponível
+
+        cpf_passageiro = input("Digite o CPF do passageiro: ")
+        # TODO verificar se CPF existe
+
+        codigo_voo = input("Digite o código do Voo: ")
+        # TODO verificar se voo existe
+
+        return {
+            "assento": assento,
+            "cpf_passageiro": cpf_passageiro,
+            "codigo_voo": codigo_voo,
+        }
+
+    def mostra_reserva(self, dados_reserva: "dict[str, str]"):
+        if dados_reserva is None:
+            print("Não há nenhuma reserva registrada ainda :( ")
+
+        else:
+            for chave, valor in dados_reserva.items():
+                print(f"{chave}: {valor}")
+
+    def seleciona_reserva(self) -> str:
+        codigo_reserva = input("Insira o código da reserva que deseja buscar: ")
+
+        return codigo_reserva
