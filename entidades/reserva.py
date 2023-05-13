50c96898044264d5a5ba2953054d6618f96db51d
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-05-13 01:54:18 -0300

    feat: implementacao da entidade `Reserva`

--- entidades/reserva.py
+++ entidades/reserva.py
@@ -1,15 +1,34 @@
+from helpers.gerador_id import gerador_id
+
+
 class Reserva:
-    def __init__(self, assento, passageiro, voo) -> None:
-        # gerar randomicamente
-        self.__codigo = 2
+    # TODO tipar os parâmetros passageiro e voo
+    def __init__(self, assento: str, passageiro, voo):
+        self.__codigo = gerador_id()
         self.__assento = assento
         self.__passageiro = passageiro
         self.__voo = voo
 
     @property
-    def codigo(self):
+    def codigo(self) -> str:
         return self.__codigo
 
     @property
-    def assento(self):
+    def assento(self) -> str:
         return self.__assento
+
+    @property
+    def passageiro(self):
+        return self.__passageiro
+
+    @property
+    def voo(self):
+        return self.__voo
+
+    @assento.setter
+    def assento(self, assento: str):
+        self.__assento = assento
+
+    @passageiro.setter
+    def passageiro(self, passageiro):
+        self.__passageiro = passageiro
