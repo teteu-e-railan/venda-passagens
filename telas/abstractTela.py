9e1b9c21f872c437fd690b8048132a61f4cddc83
RAILANABREU <railandeabreu@gmail.com>
2023-05-14 23:48:02 -0300

    refactor: inclusão do confirma opção

--- telas/abstractTela.py
+++ telas/abstractTela.py
@@ -50,3 +50,13 @@ class AbstractTela(ABC):
     def mostra_mensagem(self, mensagem: str):
         """Mostra uma mensagem qualquer."""
         print(mensagem)
+
+    def confirma_opcao(self, mensagem: str) -> bool:
+        while True:
+            try:
+                opcao = input(mensagem + " (S/N) ").upper().strip()
+                if opcao not in ["S", "N"]:
+                    raise Exception
+                return opcao == "S"
+            except Exception:
+                print("Opção inválida!")
