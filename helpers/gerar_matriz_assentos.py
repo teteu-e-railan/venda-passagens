8eafb513cf21ce3294fcee0071818e195d28185e
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-05-15 19:21:13 -0300

    fix: gerador de matriz de assentos

--- helpers/gerar_matriz_assentos.py
+++ helpers/gerar_matriz_assentos.py
@@ -1,2 +1,2 @@
 def gerar_matriz_assentos(fileiras: int, assentos_por_fileira: int):
-    return [["" for _ in range(fileiras)] for _ in range(assentos_por_fileira)]
+    return [["" for _ in range(assentos_por_fileira)] for _ in range(fileiras)]
