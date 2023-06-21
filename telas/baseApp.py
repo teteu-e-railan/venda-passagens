267b4b9ec8dee6ee043914ab5a1c9e0f0ae12d54
Matheus Pissaia <pissaia.matheus03@gmail.com>
2023-06-21 02:28:55 -0300

    test: testando funcionalidades de botoes e retorno de valores

--- telas/baseApp.py
+++ telas/baseApp.py
@@ -5,7 +5,30 @@ customtkinter.set_default_color_theme(
     "dark-blue"
 )  # Themes: "blue" (standard), "green", "dark-blue"
 
-app = customtkinter.CTk()
-app.geometry("1100x580")
-app.title("CustomTkinter simple_example.py")
-app.mainloop()
+
+class App(customtkinter.CTk):
+    def __init__(self):
+        super().__init__()
+
+        # Montagem da tela e componentes
+        self.title("Exemplo teste")
+        self.geometry(f"{1100}x{580}")
+
+        # Testando a renderizacao de botoes dentro de um `for loop`
+        for i in [0, 1, 2, 3]:
+            customtkinter.CTkButton(
+                self, text=f"Botao{i}", command=lambda index=i: self.on_click(index)
+            ).pack(padx=20, pady=10)
+
+    def mostra_opcoes(self):
+        self.mainloop()
+
+        return self.index_selecionado
+
+    def on_click(self, index: int):
+        self.index_selecionado = index
+        self.destroy()
+
+
+app = App()
+print(app.mostra_opcoes())
